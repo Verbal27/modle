90a288f710f097c93eb18bbcd2a3b82b44eb01d0
Sam Gluck <sdgluck@gmail.com>
2018-11-19 17:18:06 +0000

    wip

--- scripts/group-messages.js
+++ scripts/group-messages.js
@@ -8,39 +8,27 @@ const { sync: mkdirpSync } = require('mkdirp');
 
 // relative to project root
 const MESSAGES_PATTERN = './build/messages/**/*.json';
-const LANG_DIR         = './src/lang/';
+const LANG_DIR = './src/lang';
 
 // Groups the default messages by namespace that were extracted from the example
 // app's React components via the React Intl Babel plugin. An error will be
 // thrown if there are messages in the same namespace use the same `id`. The
 // result is a collection of namespaced collections of `id: message` pairs for
 // the app's default locale, "en-US".
-let namespacedMessages = globSync(MESSAGES_PATTERN)
-  .map((filename) => fs.readFileSync(filename, 'utf8'))
-  .map((file) => JSON.parse(file))
-  .reduce((collections, descriptors) => {
+const messages = globSync(MESSAGES_PATTERN)
+  .map((filename) => JSON.parse(fs.readFileSync(filename, 'utf8')))
+  .reduce((messages, descriptors) => {
     descriptors.forEach(({id, defaultMessage}) => {
-      let namespace  = id.split('.')[0];
-      let collection = collections[namespace];
-
-      if (!collection) {
-        collection = collections[namespace] = {};
-      }
-
-      if (collection.hasOwnProperty(id)) {
+      if (messages.hasOwnProperty(id)) {
         throw new Error(`Duplicate message id: ${id}`);
       }
-
-      collection[id] = defaultMessage;
+      messages[id] = defaultMessage;
     });
-
-    return collections;
+    return messages;
   }, {});
 
-Object.keys(namespacedMessages).forEach((namespace) => {
-  let collection = namespacedMessages[namespace];
-  let filename = path.join(LANG_DIR, namespace, 'en-US.json');
+const filename = path.join(LANG_DIR, 'en-GB.json');
+const data = JSON.stringify(messages, null, 2);
 
-  mkdirpSync(path.dirname(filename));
-  fs.writeFileSync(filename, JSON.stringify(collection, null, 2));
-});
+mkdirpSync(path.dirname(filename));
+fs.writeFileSync(filename, data);
